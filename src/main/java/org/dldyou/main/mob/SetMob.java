1b3fce019e222a4e1af0578b7c18547e15c660fd
dldyou <60469410+dldyou@users.noreply.github.com>
2023-08-22 17:16:57 +0900

    add elite skeleton

--- src/main/java/org/dldyou/main/mob/SetMob.java
+++ src/main/java/org/dldyou/main/mob/SetMob.java
@@ -20,27 +20,28 @@ import net.md_5.bungee.api.ChatColor;
 public class SetMob implements Listener {
     
     public void spawnEliteZombie(Location loc) {
-		setEliteZombieStat((LivingEntity) loc.getWorld().spawnEntity(loc, EntityType.ZOMBIE));
+		setEliteMobStat((LivingEntity) loc.getWorld().spawnEntity(loc, EntityType.ZOMBIE), "정예 좀비");
 	}
-	public void setEliteZombieStat(LivingEntity entity) {
+	public void spawnEliteSkeleton(Location loc) {
+		setEliteMobStat((LivingEntity) loc.getWorld().spawnEntity(loc, EntityType.SKELETON), "정예 스켈레톤");
+	}
+	public void setEliteMobStat(LivingEntity entity, String name) {
 		Random random = new Random();
-    	entity.setCustomName("정예 좀비");
+    	entity.setCustomName(name);
     	entity.setCustomNameVisible(false);
     	entity.setMaxHealth(50.0 + random.nextDouble(50.0));
     	entity.setHealth(entity.getMaxHealth());
     	entity.getEquipment().setHelmet(new ItemStack(Material.JACK_O_LANTERN));
-    	entity.getEquipment().setItemInMainHand(new ItemStack(Material.WOODEN_SWORD));
     	entity.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).setBaseValue(8.0);
     	entity.getAttribute(Attribute.GENERIC_ARMOR).setBaseValue(10.0);
     	entity.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.11);
     }
-	
 	@EventHandler
 	public void getMobDead(EntityDeathEvent e) {
 		e.setDroppedExp(50 + (int) e.getEntity().getMaxHealth());
 		Entity entity = (Entity) e.getEntity();
 		World w = entity.getWorld();
-		if(e.getEntity().getCustomName() == "정예 좀비") {
+		if(e.getEntity().getCustomName() == "정예 좀비" || e.getEntity().getCustomName() == "정예 스켈레톤") {
 			Random random = new Random();
 			int r = random.nextInt(100);
 			if(r < 3) {
@@ -65,6 +66,11 @@ public class SetMob implements Listener {
 		if (e.getEntityType().equals(EntityType.ZOMBIE)) {
 			if (random.nextInt(1000) > 35) {
 				spawnEliteZombie(e.getLocation());
+			}
+		}
+		else if (e.getEntityType().equals(EntityType.SKELETON)) {
+			if (random.nextInt(1000) > 35) {
+				spawnEliteSkeleton(e.getLocation());
 			}
 		}
 	}
